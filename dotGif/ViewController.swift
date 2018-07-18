ae99f2b3a750a39076736f86d26c594ea46c6fcd
Adam Moore <adammoore@adams-macbook-pro.local>
2018-07-18 16:07:03 -0600

    Initial Commit

---
+++ dotGif/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dotGif
+//
+//  Created by Adam Moore on 7/18/18.
+//  Copyright © 2018 Adam Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
